37953bf262ed2283da8510c4b1c7a9cafbddb587
Odjaime Thalmo <odjaimethalmo@hotmail.com>
2017-06-15 00:24:12 -0300

    Create database.php
    another alter

--- database.php
+++ database.php
@@ -10,7 +10,7 @@ class Conexao{
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=rema', 'root', 'c0c@!$',
+            self::$instance = new PDO('mysql:host=hostname;dbname=', 'user', 'password',
  			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
